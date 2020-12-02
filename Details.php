68cdbaed9d52c08b02efa22917d62d74b80510b3
Necis Orcus <jannick@vardevej.dk>
2020-12-02 09:41:04 +0100

    added test script

--- Details.php
+++ Details.php
@@ -1,17 +1,21 @@
-<?php
-
-namespace ipinfo\ipinfo;
-
-/**
- * Holds formatted data for a single IP address.
- */
-class Details
-{
-    public function __construct($raw_details)
-    {
-        foreach ($raw_details as $property => $value) {
-            $this->$property = $value;
-        }
-        $this->all = $raw_details;
-    }
+function getClientIP(){
+  if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+  } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+  } else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+  }
+  return $ip;
 }
+
+$ipaddress = getClientIP();
+
+function ip_details($ip) {
+  $json = file_get_contents("http://ipinfo.io/{$ip}/geo");
+  $details = json_decode($json, true);
+  return $details;
+}
+
+$details = ip_details($ipaddress);
+echo $details['city'];
